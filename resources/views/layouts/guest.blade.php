138050a659b7edebc4f557692017ad598a4679d3
GR34TNULL <gr34tnull@GR34TNULL.local>
2021-03-31 11:59:41 +0800

    fb chat

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -162,5 +162,28 @@
             //     e.preventDefault();
             // });
         </script>
+        <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution="setup_tool"
+        page_id="110695007778643">
+      </div>
     </body>
 </html>
